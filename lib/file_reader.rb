ca1471d197d676e88acf776bd6813ca7e487e98f
Mihndim <jelsieprisca2016@gmail.com>
2021-03-13 17:54:55 -0500

    Add file_reader class

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,1 +1,19 @@
 require 'colorize'
+
+class FileReader 
+  attr_reader :file_path, :file_content, :file_lines, :error_message 
+  def initialize(file_path)
+    @file_path = file_path
+    @error_message = ''
+    begin
+      @file_content = File.readlines(@file_path)
+      @file_lines = @file_content.size
+    rescue StandardError => exception
+      @file_content = []
+      @error_message = "File path incorrect, check you file path and try again\n".colorize
+      
+    else
+      
+    end
+  end 
+end
