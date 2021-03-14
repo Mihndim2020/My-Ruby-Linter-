daed05b1c3d09c192f080bcc09cde73af6d92b9b
Mihndim <jelsieprisca2016@gmail.com>
2021-03-14 06:15:59 -0400

    Initialize the ErrorChecker class

--- lib/error_checker.rb
+++ lib/error_checker.rb
@@ -1,3 +1,13 @@
 require 'colorize'
 require 'strscan'
 require_relative 'file_reader.rb'
+
+class ErrorChecker
+  attr_reader :check_errors, :errors
+
+  def initialize(file_path)
+    @check_errors = FileReader.new(file_path)
+    @errors = []
+    @keywords = %w[begin case class def do if module unless while]
+  end
+end 
