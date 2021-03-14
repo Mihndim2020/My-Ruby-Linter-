d264f462bd91e75fed0c4d99c38938296d77d1e0
Mihndim <jelsieprisca2016@gmail.com>
2021-03-14 17:43:10 -0400

    Make main.rb executable

--- lib/error_types.rb
+++ lib/error_types.rb
@@ -1,53 +1,64 @@
+# frozen_string_literal: true
+
 require 'colorize'
 require 'strscan'
 require_relative 'file_reader.rb'
 
-module ErrorTypes 
-  
+module ErrorTypes
   def indentation_error(str, idx, exp_val, msg)
     strip_line = str.strip.split(' ')
     str_match = str.match(/^\s*\s*/)
     end_check = str_match[0].size.eql?(exp_val.zero? ? 0 : exp_val - 2)
 
-    if str.strip.eql?('end') || strip_line.first == 'elsif' || strip_line.first == 'when' 
-      log_error_message("line:#{idx + 1} #{msg}") unless end_check 
+    if str.strip.eql?('end') || strip_line.first == 'elsif' || strip_line.first == 'when'
+      log_error_message("line:#{idx + 1} #{msg}") unless end_check
     elsif !str_match[0].size.eql?(exp_val)
-      log_error_message("line:#{idx + 1} #{msg}")  
-    end  
+      log_error_message("line:#{idx + 1} #{msg}")
+    end
   end
 
   def tag_errors(*args)
-    @check_errors.file_content.each_with_index do |str, idx|
+    @check_errors.file_content.each_with_index do |str, _idx|
       open_paren = []
       close_paren = []
       open_paren << str.scan(args[0])
       close_paren << str.scan(args[1])
 
-    status = open_paren.flatten.size <=> close_paren.flatten.size
+      status = open_paren.flatten.size <=> close_paren.flatten.size
 
-    log_error_message("line:#{index + 1} Lint/Syntax: Unexpected/Missing token '#{args[2]}' #{args[4]}") if status.eql?(1)
-    log_error_message("line:#{index + 1} Lint/Syntax: Unexpected/Missing token '#{args[3]}' #{args[4]}") if status.eql?(-1)
+      if status.eql?(1)
+        log_error_message("line:#{index + 1} Lint/Syntax: Unexpected/Missing token '#{args[2]}' #{args[4]}")
+      end
+      if status.eql?(-1)
+        log_error_message("line:#{index + 1} Lint/Syntax: Unexpected/Missing token '#{args[3]}' #{args[4]}")
+      end
     end
   end
 
   def check_end_empty_line(str, idx)
     return unless str.strip.split(' ').first.eql?('end')
 
-    log_error_message("line:#{idx} Extra empty line detected at the end of the block body") if @check_errors.file_content[idx - 1].strip.empty?
+    if @check_errors.file_content[idx - 1].strip.empty?
+      log_error_message("line:#{idx} Extra empty line detected at the end of the block body")
+    end
   end
 
   def check_empty_line_do_block(str, idx)
     message = 'Extra empty line detected at the beginning of the block'
     return unless str.strip.split(' ').include?('do')
 
-    log_error_message("line:#{idx + 2} #{message}") if @check_errors.file_content[indx + 1].strip.empty?  
+    if @check_errors.file_content[indx + 1].strip.empty?
+      log_error_message("line:#{idx + 2} #{message}")
+    end
   end
 
   def check_class_empty_line(str, idx)
     msg = 'Extra empty line detected at class body beginning'
     return unless str.strip.split(' ').first.eql?('class')
 
-    log_error_message("line:#{idx + 2} #{msg}") if @check_errors.file_content[idx + 1].strip.empty?
+    if @check_errors.file_content[idx + 1].strip.empty?
+      log_error_message("line:#{idx + 2} #{msg}")
+    end
   end
 
   def check_def_empty_line(str, idx)
@@ -56,8 +67,12 @@ module ErrorTypes
 
     return unless str.strip.split(' ').first.eql?('def')
 
-    log_error_message("line:#{idx + 2} #{message1}") if @check_errors.file_content[idx + 1].strip.empty?
-    log_error_message("line:#{idx + 1} #{message2}") if @check_errors.file_content[indx - 1].strip.split(' ').first.eql?('end')
+    if @check_errors.file_content[idx + 1].strip.empty?
+      log_error_message("line:#{idx + 2} #{message1}")
+    end
+    if @check_errors.file_content[indx - 1].strip.split(' ').first.eql?('end')
+      log_error_message("line:#{idx + 1} #{message2}")
+    end
   end
 
   def log_error_message(error_msg)
--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'colorize'
 
-class FileReader 
-  attr_reader :file_path, :file_content, :file_lines, :error_message 
+class FileReader
+  attr_reader :file_path, :file_content, :file_lines, :error_message
   def initialize(file_path)
     @file_path = file_path
-    @error_message = ''
+    @error_message = ' '
     begin
       @file_content = File.readlines(@file_path)
-      @file_lines = @file_content.size 
-    rescue StandardError => exception
+      @file_lines = @file_content.size
+    rescue StandardError => e
       @file_content = []
-      @error_message = "File path incorrect, check you file path and try again\n".colorize(:red)  
+      @error_message = "File path incorrect, check you file path and try again\n".colorize(:red)
     end
-  end 
+  end
 end
