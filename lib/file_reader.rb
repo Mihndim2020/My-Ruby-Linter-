101016cc8563a3b1d56cf787b04111f208335d60
Mihndim <jelsieprisca2016@gmail.com>
2021-03-15 15:31:29 -0400

    Use string concatenation to out no offense in main, Remove empty space in file_reader

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -9,7 +9,7 @@ class FileReader
     begin
       @file_content = File.readlines(@file_path)
       @file_lines = @file_content.size
-    rescue StandardError 
+    rescue StandardError
       @file_content = []
       @error_message = "File path incorrect, check you file path and try again\n".colorize(:red)
     end
