9b3dd7a49134bdeee87d177a5530a8eb2b46e46d
Mihndim <jelsieprisca2016@gmail.com>
2021-03-15 20:38:43 -0400

    Add test on check_indentation class

--- spec/error_checker_spec.rb
+++ spec/error_checker_spec.rb
@@ -11,4 +11,11 @@ describe ErrorChecker do
   end
 
   context '#check_indentation' do
+    example 'should return indentation errror on line 28' do
+      error_cherker.check_indentation
+      expect(error_cherker.errors[5]).to eql('line:28 Use two spaces for indentation')
+    end  
+  end
+
+  context '#tag_error' do
 end
